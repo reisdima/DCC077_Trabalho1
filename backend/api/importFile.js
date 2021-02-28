99e6c44831aedfeefc4dd69a65da8c08586c6f4b
Joao Victor Balboa <jbalboa@ice.ufjf.br>
2021-02-28 19:42:25 -0300

    corrige chave para óbito

--- backend/api/importFile.js
+++ backend/api/importFile.js
@@ -6,7 +6,7 @@ module.exports = app => {
     const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
 
     const importRegistro = async (req, res) => {
-        const descricao = ["examinado", "diagnosticado", "internado", "óbito", "recuperado"];
+        const descricao = ["examinado", "diagnosticado", "internado", "obito", "recuperado"];
         app.elasticsearch
             .search({
                 index: 'ministerio_saude',
